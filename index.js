0e5379182743615a1ee383face93458f8fe87509
Krzysztof Podmokly <krzysztof.podmokly@visma.com>
2019-06-19 09:42:21 +0200

    Adding npm package concurrently - merging front & backend server

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ authRoutes(app);
 // require('./routes/authRoutes')(app) => alternative way
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Listening from ${PORT}`);
